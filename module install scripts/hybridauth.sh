a859167946c8f2513e79c2dae7c9b91486e6398b
nishikawa daisaku <kamihicouki@gmail.com>
2017-02-04 19:07:53 +0900

    add "rename_admin_paths" module

--- module install scripts/hybridauth.sh
+++ module install scripts/hybridauth.sh
@@ -1,7 +1,9 @@
-drush dl hybridauth ctools -y
+drush dl hybridauth-7.x-2.x-dev ctools -y
 DIR=/var/www/html/sites/all/libraries/hybridauth
+rm -r ${DIR}
 mkdir -p ${DIR}
-curl -sL https://github.com/hybridauth/hybridauth/archive/master.tar.gz | tar xz -C ${DIR} --strip=1
+curl -sL https://github.com/hybridauth/hybridauth/archive/v2.7.0.tar.gz | tar xz -C ${DIR} --strip=1
 drush en hybridauth -y
+#drush role-add-perm 'anonymous' 'use hybridauth'
 drush dl realname -y
 drush en realname -y
---
+++ module install scripts/rename_admin_paths.sh
@@ -0,0 +1,3 @@
+drush dl rename_admin_paths -y
+drush en rename_admin_paths -y
+#admin/config/user-interface/rename-admin-paths

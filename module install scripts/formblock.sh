cbad32c832c321208f077d9f67aca568fed367ab
nishikawa daisaku <kamihicouki@gmail.com>
2017-02-07 17:27:56 +0900

    update formblock

--- module install scripts/formblock.sh
+++ module install scripts/formblock.sh
@@ -1,2 +1,10 @@
+エラーが出るため没
+# Notice: Undefined index: media node_add() (/var/www/html/modules/node/node.pages.inc ファイル　77行).
+# Notice: Trying to get property of non-object node_add() (/var/www/html/modules/node/node.pages.inc ファイル　77行).
+# Notice: Undefined index: media_node_form drupal_retrieve_form() (/var/www/html/includes/form.inc ファイル　807行).
+# Warning: call_user_func_array() expects parameter 1 to be a valid callback, function 'media_node_form' not found or invalid function name drupal_retrieve_form() (/var/www/html/includes/form.inc ファイル　842行).
+# Notice: Trying to get property of non-object formblock_get_block() (/var/www/html/sites/all/modules/formblock/formblock.module ファイル　145行).
+
 drush dl formblock -y
+
 drush en formblock -y
